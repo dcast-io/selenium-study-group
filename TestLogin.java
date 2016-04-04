ff70586383b8fd0331d4b750a8481e04d9a4b4f3
Jennifer Galvin <me@jennifer-galvin.com>
2016-04-04 19:50:41 -0400

    Create TestLogin.java

---
+++ TestLogin.java
@@ -0,0 +1,32 @@
+//filename: tests/TestLogin.java
+ package tests;
+
+ import org.junit.Test;
+ import org.junit.Before;
+ import org.junit.After;
+ import org.openqa.selenium.By;
+ import org.openqa.selenium.WebDriver;
+ import org.openqa.selenium.firefox.FirefoxDriver;
+
+   public class TestLogin {
+      private WebDriver driver;
+
+    @Before
+       public void setUp() {
+           driver = new FirefoxDriver();
+    }
+
+    @Test
+       public void succeeded() {
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button")).click();
+    }
+
+    @After
+       public void tearDown() {
+        driver.quit();
+    }
+
+}
